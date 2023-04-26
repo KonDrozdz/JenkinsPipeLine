6f6d378b2cb645633a36e392fe8e58086e71e8a6
KonDrozdz <konrdro873@student.polsl.pl>
2023-04-26 10:52:24 +0200

    init

---
+++ src/main/java/PipeLine/demo/JenkinsPipeLineApplication.java
@@ -0,0 +1,13 @@
+package PipeLine.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipeLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipeLineApplication.class, args);
+	}
+
+}
